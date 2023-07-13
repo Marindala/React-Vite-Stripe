eb59a8dc24e3aa8a6c1777cd82858dad40b70393
Marina <95050756+Marindala@users.noreply.github.com>
2023-07-13 17:06:48 -0300

    form stripe

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 //import { useState } from 'react'
+import { loadStripe } from "@stripe/stripe-js";
+import {Elements} from "@stripe/react-stripe-js"
+import "./App.css";
 
-import './App.css'
+const stripePromise = loadStripe(
+  "pk_test_51NTVWGLzLbrIDt2zcA1sj5gjWOi9qY7AicLOx0Ig311DdLvKJiXANQM4ngtla9dawvpKNaJjhj8efHMD9V8TEx1S00Rtxr4SJ5"
+); //key public in front
 
-function App() {
-  
-
-  return (
-    <>
-      <div className="h1">Hello Soy yo</div>
-    </>
-  )
+const CheckoutForm = () => {
+  return <form></form>
 }
 
-export default App
+function App() {
+  return (
+    <>
+   <Elements stripe={stripePromise}>
+   <CheckoutForm/>
+   </Elements>
+    </>
+  );
+}
+
+export default App;
