3da1ee42de3c495da96a1f16412b858406fb92fa
Marina <95050756+Marindala@users.noreply.github.com>
2023-07-19 01:56:22 -0300

    .env in vite key of test

--- server/index.js
+++ server/index.js
@@ -1,19 +1,15 @@
 /* impor express = require ('express'); */
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import Stripe  from 'stripe';
-import  cors from 'cors';
-//import { env } from 'node:process';
-/* const {
-  API_KEY
-} =process.env; */
- 
-const app = express()
+import express from "express";
+import Stripe from "stripe";
+import cors from "cors";
 
-//stripe("sk_test_51NTVWGLzLbrIDt2zXGv3XtfMDOIQOcHuNJawmu8VMCzzJLRah8hdDb85hZIJr4UzJYwI2clZA1e01ACOAvVgxWzS001CA3MKKs")
-const stripe = new Stripe("sk_test_51NTVWGLzLbrIDt2zXGv3XtfMDOIQOcHuNJawmu8VMCzzJLRah8hdDb85hZIJr4UzJYwI2clZA1e01ACOAvVgxWzS001CA3MKKs")
+import { VITE_API_KEY } from "import.meta.env";
+const app = express();
+
+const stripe = new Stripe(VITE_API_KEY);
 
 // Middleware para habilitar CORS
 /*  app.use((req, res, next) => {
@@ -23,7 +19,7 @@ const stripe = new Stripe("sk_test_51NTVWGLzLbrIDt2zXGv3XtfMDOIQOcHuNJawmu8VMCzz
     next();
   });  */
 
-app.use(cors({ origin: "http://127.0.0.1:5173" }));//para que acepte la info del front
+app.use(cors({ origin: "http://127.0.0.1:5173" })); //para que acepte la info del front
 
 app.use(express.json());
 
@@ -44,23 +40,21 @@ app.post("/api/checkout", async (req, res) => {
     return res.status(200).json({ message: "Successful Payment" });
   } catch (error) {
     console.log(error);
-    return res.json({ message: error.raw.message  }); //in objects error
+    return res.json({ message: error.raw.message }); //in objects error
   }
 });
-    // Lógica para procesar el pago
+// Lógica para procesar el pago
 
-    // Si todo está bien, envía una respuesta JSON
- /*    return res.json({ message: "Pago exitoso" });
+// Si todo está bien, envía una respuesta JSON
+/*    return res.json({ message: "Pago exitoso" });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Hubo un error en el servidor" });
   }
 }); */
 
-
-
-app.listen(3001, () =>{
-    console.log("Server on port", 3001)
-})
+app.listen(3001, () => {
+  console.log("Server on port", 3001);
+});
 
 /* El error in server que estás viendo indica que estás utilizando la sintaxis require en un archivo que está siendo tratado como un módulo ES (ECMAScript) debido a la configuración en el archivo package.json. A partir de ECMAScript 6 (ES6), se introdujo una nueva forma de importar y exportar módulos usando las palabras clave import y export, en lugar de require y module.exports utilizados en CommonJS. */
